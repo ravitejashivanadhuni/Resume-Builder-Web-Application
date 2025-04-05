4ec90725bdcf4e66f43a23ab00a96455125d83b7
Raviteja Shivanadhuni <ravitejashivanadhuni@gmail.com>
2025-04-05 23:17:39 +0530

    Update vite.config.js

--- Resume_builder11/frontend/vite.config.js
+++ Resume_builder11/frontend/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      "@": path.resolve(__dirname, "./src"),  // ✅ allows @/component/... etc.
       "react": "react",
       "react-dom": "react-dom"
     }
