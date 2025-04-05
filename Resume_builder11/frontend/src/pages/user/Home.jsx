3e772f17e72d1e41dd35d7a8d3915065c1334b97
Raviteja Shivanadhuni <ravitejashivanadhuni@gmail.com>
2025-04-05 23:15:42 +0530

    Update Home.jsx

--- Resume_builder11/frontend/src/pages/user/Home.jsx
+++ Resume_builder11/frontend/src/pages/user/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import GoogleAd from "../../../component/GoogleAd";
+import GoogleAd from "@/component/GoogleAd";
 
 const Home = () => {
   const [isVisible, setIsVisible] = useState(false);
